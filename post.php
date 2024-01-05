19dec08f649599eeb62e801f603817535a3e03c5
balltheerayut <152575191+balltheerayut@users.noreply.github.com>
2024-01-05 11:02:59 +0700

    work4 webboard
    work4 webboard

---
+++ post.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webboard</title>
+</head>
+<body>
+    <h1 style="text-align: center;">WebboardlnwZa007</h1>
+    <hr>
+    <div style="text-align: center ;">ต้องการดูกระทู้หมายเลข<?php echo $_GET['id'];?></div>
+    <br>
+    <table style="border: 2px solid black; width:40%;" align="center">
+        <tr style="background-color:#6CD2FE ;"><td>แสดงควาคิดเห็น</td></tr>
+        <tr><td align="center"><textarea cols="50"rows="10"></textarea><br>
+        <input type="submit" value="ส่งข้อความ"></td></tr>
+    </table>
+    <br>
+    <div style="text-align: center;"><a href="index.html">กลับไปหน้าหลัก
+</body>
+</html>
